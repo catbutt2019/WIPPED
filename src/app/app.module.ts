951ea1b148106998db564c2a69a8c680fa018bcf
catbutt2019 <c13453402@mydit.ie>
2019-07-04 20:31:30 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,15 +8,30 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { environment } from 'src/environments/environment';
+import { AuthenticationService } from './services/authentication.service';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+import * as firebase from 'firebase';
+import { ReactiveFormsModule } from '@angular/forms';
+
+firebase.initializeApp(environment.firebase);
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule,
+     IonicModule.forRoot(),
+      AppRoutingModule,
+      ReactiveFormsModule,
+      AngularFireAuthModule
+    ],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    AuthenticationService,
+    { provide: RouteReuseStrategy,
+      useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
 })
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-};
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyChkwA9qwuX8DWJb5m0Gjnsiqgsj0377Uc",
+    authDomain: "wippy-1962c.firebaseapp.com",
+    databaseURL: "https://wippy-1962c.firebaseio.com",
+    projectId: "wippy-1962c",
+    storageBucket: "wippy-1962c.appspot.com",
+    messagingSenderId: "115626323332",
+    appId: "1:115626323332:web:84270a7a02c1607b"
+  }
+ };
 
 /*
  * For easier debugging in development mode, you can import the following file
